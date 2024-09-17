032ba3a89c55df8641e9f8fb8195eb7ec5f2f1f0
AddyVeerendra <addyveerendra@gmail.com>
2024-09-17 19:52:15 -0400

    Limelight Neural Detector testing
    Creation of the Limelight class which contains Limelight functions, and the LimelightNeuralDetectorTest, which will help us test how the detector works.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareClasses/Limelight.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.HardwareClasses;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+import java.util.List;
+
+public class Limelight {
+
+    private final Limelight3A limelight;
+
+    public Limelight(HardwareMap hardwareMap) {
+        // Initialize the Limelight object using the HardwareMap
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+    }
+
+    // Start the Limelight
+    public void start() {
+        limelight.start();
+    }
+
+    // Stop the Limelight
+    public void stop() {
+        limelight.stop();
+    }
+
+    // Get the Limelight's latest horizontal angle (tx)
+    public double getTx() {
+        return limelight.getLatestResult().getTx();
+    }
+
+    // Get the pose of the robot (botpose) from the latest result
+    public Pose3D getBotpose() {
+        return limelight.getLatestResult().getBotpose();
+    }
+
+    // Get detector results (e.g., for object detection)
+    public List<LLResultTypes.DetectorResult> getDetectorResults() {
+        return limelight.getLatestResult().getDetectorResults();
+    }
+
+    public void pipelineSwitch(int i) {
+        limelight.pipelineSwitch(i);
+    }
+
+    public boolean isConnected() {
+        return limelight.isConnected();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Limelight/LimelightNeuralDetectorTest.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.Limelight;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.HardwareClasses.Limelight;
+
+@Autonomous(name = "LimelightDetectorTest", group = "Sensor")
+public class LimelightNeuralDetectorTest extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+        Limelight limelight = new Limelight(hardwareMap);
+
+        telemetry.setMsTransmissionInterval(11);
+        limelight.pipelineSwitch(3);
+        limelight.start();
+
+        telemetry.addData(">", "Robot Ready. Press Play.");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // Retrieve status and other necessary information from the Limelight
+            if (limelight.isConnected()) {
+                limelight.getDetectorResults().forEach(dr -> {
+                    if ("bluesample".equals(dr.getClassName())) {
+                        telemetry.addData("Blue Sample", "Area: %.2f, targetXDegrees: %.2f, targetYDegrees: %.2f",
+                                dr.getTargetArea(), dr.getTargetXDegrees(), dr.getTargetYDegrees());
+                    }
+                });
+            } else {
+                telemetry.addData("Status", "Limelight Not Connected");
+            }
+
+            telemetry.update();
+        }
+    }
+}
