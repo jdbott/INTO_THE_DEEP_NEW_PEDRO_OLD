382d570d6c75137b218c9b14fcfd5f7a8e268979
AddyVeerendra <addyveerendra@gmail.com>
2024-09-29 19:14:17 -0400

    Testing distance estimation
    These programs should be able to help estimate the distance from the Limelight using some TargetCorners and real world lengths of the sample.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Limelight/LimelightNeuralDetectorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Limelight/LimelightNeuralDetectorTest.java
@@ -1,17 +1,19 @@
-package org.firstinspires.ftc.teamcode.LimeLight;
+package org.firstinspires.ftc.teamcode.Limelight;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.HardwareClasses.Limelight;
+import java.util.List;
+
 
 @TeleOp(name = "LimelightDetectorTest", group = "Sensor")
 public class LimelightNeuralDetectorTest extends LinearOpMode {
+
 
     public void runOpMode() throws InterruptedException {
         Limelight limelight = new Limelight(hardwareMap);
 
         telemetry.setMsTransmissionInterval(11);
-        // PIPELINE 1 IS THE NEURAL NETWORK DETECTOR
         limelight.pipelineSwitch(1);
         limelight.start();
 
@@ -21,12 +23,26 @@ public class LimelightNeuralDetectorTest extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            // Retrieve status and other necessary information from the Limelight
             if (limelight.isConnected()) {
                 limelight.getDetectorResults().forEach(dr -> {
                     if ("bluesample".equals(dr.getClassName())) {
-                        telemetry.addData("Blue Sample", "Area: %.2f, targetXDegrees: %.2f, targetYDegrees: %.2f",
-                                dr.getTargetArea(), dr.getTargetXDegrees(), dr.getTargetYDegrees());
+                        List<List<Double>> targetCorners = dr.getTargetCorners();
+
+                        double diagonalPixels = DistanceEstimator.getObjectDiagonalInPixels(targetCorners);
+                        double objectWidthPixels = DistanceEstimator.getObjectWidthInPixels(targetCorners);
+                        double objectHeightPixels = DistanceEstimator.getObjectHeightInPixels(targetCorners);
+
+                        // Estimate distance using diagonal or width/height based on orientation
+                        double diagonalDistance = DistanceEstimator.estimateDistanceFromDiagonal(
+                                Math.sqrt(Math.pow(DistanceEstimator.REAL_WORLD_WIDTH, 2) + Math.pow(DistanceEstimator.REAL_WORLD_HEIGHT, 2)),
+                                diagonalPixels
+                        );
+                        double widthDistance = DistanceEstimator.estimateDistanceFromWidth(DistanceEstimator.REAL_WORLD_WIDTH, objectWidthPixels);
+                        double heightDistance = DistanceEstimator.estimateDistanceFromHeight(DistanceEstimator.REAL_WORLD_HEIGHT, objectHeightPixels);
+
+                        telemetry.addData("Distance (Diagonal)", "%.2f meters", diagonalDistance);
+                        telemetry.addData("Distance (Width)", "%.2f meters", widthDistance);
+                        telemetry.addData("Distance (Height)", "%.2f meters", heightDistance);
                     }
                 });
             } else {
